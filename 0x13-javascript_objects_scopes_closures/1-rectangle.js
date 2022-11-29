19898381dab911e1b6532f49784ebf79dba04b19
LeulBayesa <leulbayesa@gmail.com>
2022-11-29 10:28:49 -0800

    Updated

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,15 +1,8 @@
-ass Rectangle {
-
-	  constructor (w, h) {
-
-		      this.width = w;
-
-		      this.height = h;
-
-		    }
-
+class Rectangle {
+	constructor (w, h) {
+		this.width = w;
+	        this.height = h;
+		}
 }
-
-
 
 module.exports = Rectangle;
